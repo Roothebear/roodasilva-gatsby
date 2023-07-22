9d4c31709ac5b4f5950c3f3f80b5411ccf6d88c4
Roo da Silva <65015782+roothebear@users.noreply.github.com>
2023-07-22 22:36:10 +0100

    delete unused component

--- src/components/bannerImage.js
+++
@@ -1,9 +0,0 @@
-import React from "react";
-import { bannerImage } from "./bannerImage.module.css";
-import { StaticImage } from "gatsby-plugin-image";
-
-const BannerImage = () => {
-  return <StaticImage className={bannerImage} />;
-};
-
-export default BannerImage;
